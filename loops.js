60bc14f2236cb9780147f23626abfee436dda801
Udaykiranbandari <udaybandari888@gmail.com>
2025-03-04 00:23:59 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,49 @@
+// question-1
+
+for (a =10;a<=10;a++){
+    console.log(a)
+}
+ 
+// question-2
+sum = 0
+for(b = 1;b<=100;b++){
+    sum +=b
+}
+console.log(sum)
+
+// question-3
+
+for (c = 1 ; c<=10;c++){
+    console.log(`7*${c} = ${c*7}`)
+}
+
+// question-4
+
+for (d = 2;d<=20;d=d+2){
+    console.log(d)
+}
+
+// question-5
+
+var mul = 1
+for (var e = 1;e<=5;e++){
+    mul*=e
+}
+console.log(mul)
+
+// question-6
+
+let base = 2
+let power =5
+let res=1
+for (var f =1;f<=power;f++){
+    res*=base
+}
+console.log(res)
+
+
+// question-7
+
+for(var g =20;g>=1;g--){
+    console.log(g)
+}

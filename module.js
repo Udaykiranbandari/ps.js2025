6f34bc96196ba2a05fd9d4c0d6895deede3945d9
Udaykiranbandari <udaybandari888@gmail.com>
2025-04-08 15:51:14 +0530

    Add files via upload

---
+++ module.js
@@ -0,0 +1,5 @@
+import {message,add,multiply,sub} from "./module1.js";
+console.log(message())
+console.log(add(2,3))
+console.log(multiply(3,4))
+console.log(sub(44,22))
---
+++ module1.js
@@ -0,0 +1,22 @@
+export const message =()=>{
+    const name = "uday"
+    const  age = 23;
+    return name + " is " +age+"years old"
+}
+
+ export const add = (a, b) => a + b;
+
+  export const multiply = (x, y) => {
+    const result = x * y;
+    return result;
+  };
+
+ export const sub = (a,b)=>{
+  const result =a-b;
+  return result;
+ }
+  
+  
+
+  
+

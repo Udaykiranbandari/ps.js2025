5203224f8768d43f7ecd112eeeeb2b014d29c6f6
Udaykiranbandari <udaybandari888@gmail.com>
2025-04-09 12:31:19 +0530

    Add files via upload

---
+++ 07.04.2025.js
@@ -0,0 +1,42 @@
+// string methods
+let name = "hello";
+let upperName = name.toUpperCase(); 
+console.log(upperName); 
+// Output: "HELLO"
+
+let text = "Hello";
+console.log(text.length);
+// Output: 5
+
+let name1 = "JOHN";
+console.log(name1.toLowerCase()); 
+// Output: "john"
+
+let str = "JavaScript is fun";
+console.log(str.includes("fun"));
+ // Output: true
+
+ let str1 = "banana";
+console.log(str1.indexOf("a")); 
+// Output: 1
+
+let str2 = "apple,banana,orange";
+console.log(str2.split(",")); 
+// Output: ["apple", "banana", "orange"]
+
+
+let str3 = "Hello";
+console.log(str3.charAt(1)); 
+// Output: "e"
+
+let str4 = "JavaScript";
+console.log(str4.startsWith("Java"));
+ // Output: true
+
+ let str5 = "photo.jpg";
+console.log(str5.endsWith(".jpg")); 
+// Output: true
+
+
+
+
